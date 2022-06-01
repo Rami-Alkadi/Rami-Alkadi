4520b85a4aa3bbc549618df5131d7a940280fa4c
Rami-Alkadi <90176973+Rami-Alkadi@users.noreply.github.com>
2022-06-01 14:54:42 +0200

    my webdeveloper website

---
+++ webdev-website/x = 2.py
@@ -0,0 +1,9 @@
+x = input("how old are you :")
+if x == (1932,1933,1934,1935,1935): 
+    print("Vaccine registration : From 25 January 2021, Vaccine: BioNTech, Location : Groningen  ")
+elif x == range(1932,1936):
+    print("Vaccine registration : From 29 January 2021, Vaccine: Pfizer, Location : Arnhem ")
+elif x == range(1937-1941):
+    print("Vaccine registration : From 5 February 2021, Vaccine: Pfizer, Location : Breda")
+
+
